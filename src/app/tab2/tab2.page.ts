ea6720ffb8b5de0d64a7ee28131b0a5448068523
Luís Felipe Borges Silva <luis.felipe@cosmetologia.local>
2019-12-03 15:27:26 -0300

    pagina wifi

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,78 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  wifis = [
+    {
+      nome: "Alunos",
+      usuario: "",
+      senha: "alunoscosmet"
+    },
+    {
+      nome: "Rede",
+      usuario: "teste.01",
+      senha: "Mudar123456",
+      ip: "192.168.0.63",
+      mascara: "23",
+      dns: "192.168.0.9",
+      gateway: "192.168.0.254"
+
+    },
+    {
+      nome: "Abefarma-CEL",
+      usuario: "(Ficha do usuario)",
+      senha: "(Ficha do usuario)"
+    },
+    {
+      nome: "Consulfarma-CEL",
+      usuario: "(Ficha do usuario)",
+      senha: "(Ficha do usuario)"
+    }
+  ]
+
+  constructor(private alertController: AlertController) { }
+
+  async exibirInformacoes(wifi: any) {
+
+    let message = "";
+
+    if (wifi.usuario)
+      message += this.adicionarNovaLinhaMensagem(message, "Usuário: " + wifi.usuario);
+
+    if (wifi.senha)
+      message += this.adicionarNovaLinhaMensagem(message, "Senha: " + wifi.senha);
+
+      if (wifi.ip)
+      message += this.adicionarNovaLinhaMensagem(message, "IP: " + wifi.ip);
+
+      if (wifi.mascara)
+      message += this.adicionarNovaLinhaMensagem(message, "Mascara: " + wifi.mascara);
+
+      if (wifi.dns)
+      message += this.adicionarNovaLinhaMensagem(message, "DNS: " + wifi.dns);
+
+      if (wifi.gateway)
+      message += this.adicionarNovaLinhaMensagem(message, "GATEWAY: " + wifi.gateway);
+
+    let alert = await this.alertController.create({
+      header: wifi.nome,
+      message: message
+    });
+
+    await alert.present();
+
+  }
+
+  adicionarNovaLinhaMensagem(mensagem: string, novaLinha: string) {
+
+    if (mensagem.length == 0) {
+      return novaLinha;
+    }
+    else {
+      return "<br/>" + novaLinha;
+    }
+
+    return mensagem;
+
+  }
 
 }
