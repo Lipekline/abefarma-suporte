5d56c4316b1edf930df18a8352aec7abe499215a
Luís Felipe Borges Silva <luis.felipe@cosmetologia.local>
2019-12-10 15:57:46 -0300

    pagina impressora

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,67 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  impressora = [
+    {
+      nome: "ADM ABEFARMA",
+      ip: "192.168.0.18",
+      toner: "Toner CF226A ou CF226X"
+    },
+    {
+      nome: "Comercial Abefarma",
+      ip: "192.168.0.19",
+      toner: "Toner CF226A ou CF226X"
+
+    },
+    {
+      nome: "DIRETORIA",
+      ip: "192.168.0.15",
+      toner:"CF510 - PRETO, CF511 - AZUL, CF512A - AMARELO, CF513A - MAGENTA"
+    },
+    {
+      nome: "Comercial Consulfarma",
+      ip: "192.168.0.14",
+      toner: "Toner CF226A ou CF226X"
+    },
+    {
+      nome: "ADM Consulfarma",
+      ip:"192.168.0.17",
+      toner:"Toner CF226A ou CF226X"
+    }
+  ]
+
+  constructor(private alertController: AlertController) { }
+
+  async exibirInformacoes(impressora: any) {
+
+    let message = "";
+
+      if (impressora.ip)
+      message += this.adicionarNovaLinhaMensagem(message, "IP: " + impressora.ip);
+
+      if (impressora.toner)
+      message += this.adicionarNovaLinhaMensagem(message, "Toner: " + impressora.mascara);
+
+    let alert = await this.alertController.create({
+      header: impressora.nome,
+      message: message
+    });
+
+    await alert.present();
+
+  }
+
+  adicionarNovaLinhaMensagem(mensagem: string, novaLinha: string) {
+
+    if (mensagem.length == 0) {
+      return novaLinha;
+    }
+    else {
+      return "<br/>" + novaLinha;
+    }
+
+    return mensagem;
+
+  }
 
 }
