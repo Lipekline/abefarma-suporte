0d5cc77a59f886800869ffc566d80d74b675153a
Luís Felipe Borges Silva <luis.felipe@cosmetologia.local>
2019-12-17 15:52:34 -0300

    pagina colaboradores

--- src/app/tab4/tab4.module.ts
+++ src/app/tab4/tab4.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { Tab4PageRoutingModule } from './tab4-routing.module';
 
 import { Tab4Page } from './tab4.page';
+import { DetalhesComponent } from './detalhes/detalhes.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { Tab4Page } from './tab4.page';
     IonicModule,
     Tab4PageRoutingModule
   ],
-  declarations: [Tab4Page]
+  declarations: [Tab4Page, DetalhesComponent],
+  entryComponents: [DetalhesComponent]
 })
-export class Tab4PageModule {}
+export class Tab4PageModule { }
--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, ModalController } from '@ionic/angular';
+import { DetalhesComponent } from './detalhes/detalhes.component';
 
 @Component({
   selector: 'app-tab4',
@@ -7,9 +9,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
-  constructor() { }
+  colaboradoresAbefarma = [
+    {
+      nome: "Dougleta Jones",
+      usuariowifi: "teste",
+      senha: "teste"
+    }
+  ];
+  colaboradoresConsulfarma = [
+    {
+      nome: "Dougleta Jones",
+      usuariowifi: "teste",
+      senha: "teste"
+    },
+    {
+      nome: "Luizeta Jones",
+      usuariowifi: "teste",
+      senha: "teste"
+    },
+    {
+      nome: "Dougleta Jones",
+      usuariowifi: "teste",
+      senha: "teste"
+    },
+    {
+      nome: "Dougleta Jones",
+      usuariowifi: "teste",
+      senha: "teste"
+    }
+  ]
+
+  constructor(private alertController: AlertController, public modalController: ModalController) { }
 
   ngOnInit() {
+  }
+
+  async exibirInformacoes(colaborador: any) {
+
+    const modal = await this.modalController.create({
+      component: DetalhesComponent,
+      componentProps: { colaborador: colaborador }
+
+    });
+    return await modal.present();
+
+    // let message = "";
+
+    // if (colaborador.nome)
+    // message += this.adicionarNovaLinhaMensagem(message, "Nome:" + colaborador.nome);
+
+    //   if (colaborador.usuariowifi)
+    //   message += this.adicionarNovaLinhaMensagem(message, "Usuario WIFI:" + colaborador.usuariowifi);
+
+    //   if (colaborador.senha)
+    //   message += this.adicionarNovaLinhaMensagem(message, "Senha: " + colaborador.senha);
+
+    // let alert = await this.alertController.create({
+    //   header: colaborador.nome,
+    //   message: message
+    // });
+
+    // await alert.present();
+
+  }
+  adicionarNovaLinhaMensagem(mensagem: string, novaLinha: string) {
+
+    if (mensagem.length == 0) {
+      return novaLinha;
+    }
+    else {
+      return "<br/>" + novaLinha;
+    }
+
+    return mensagem;
+
   }
 
 }
